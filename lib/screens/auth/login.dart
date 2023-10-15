81b6fe0d212750e8a20ffe37b5fb63996962007c
NguyenH8 <nguyentran.07052002ak@gmail.com>
2023-10-15 16:24:27 +0700

    Login Screen

---
+++ lib/screens/auth/login.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project/utils/colors.dart';
+import 'package:flutter_project/utils/sized_box.dart';
+import 'package:flutter_project/widgets/text_field.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: primaryColor,
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.all(10.0),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              sizedBox,
+              const Text(
+                "Say hello to your English tutors",
+                style: TextStyle(
+                  color: secondaryColor,
+                  fontSize: 36.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              sizedBox,
+              const Text(
+                "Become fluent faster through one-on-one video chat lessons tailored to your goals.",
+                style: TextStyle(
+                  color: tertiaryColor,
+                  fontSize: 24,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              sizedBox,
+              TextFieldInput(
+                  textEditingController: _emailController,
+                  hintText: "Enter your email",
+                  textInputType: TextInputType.emailAddress),
+              sizedBox,
+              TextFieldInput(
+                textEditingController: _passwordController,
+                hintText: "Enter your password",
+                textInputType: TextInputType.text,
+                isPass: true,
+              ),
+              sizedBox,
+              InkWell(
+                onTap: () {},
+                child: const Text(
+                  "Forgot Password",
+                  style: TextStyle(
+                    color: secondaryColor,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+              subSizedBox,
+              InkWell(
+                child: Container(
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: secondaryColor,
+                  ),
+                  child: const Text("Login"),
+                ),
+              ),
+              sizedBox,
+              const Text("Or continue with"),
+              subSizedBox,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(onPressed: () {}, icon: Image.asset('public/icons/facebook.png'), iconSize: 32.0,),
+                  IconButton(onPressed: () {}, icon: Image.asset('public/icons/google.png'), iconSize: 32.0,),
+                  IconButton(onPressed: () {}, icon: Image.asset('public/icons/phone.png'), iconSize: 32.0,),
+                ],
+              ),
+              sizedBox,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: const Text(
+                      'Dont have an account?',
+                    ),
+                  ),
+                  GestureDetector(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: const Text(
+                        ' Register.',
+                        style: TextStyle(
+                          color: secondaryColor,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/sized_box.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const subSizedBox = SizedBox(height: 16,);
+const sizedBox = SizedBox(height: 24,);
