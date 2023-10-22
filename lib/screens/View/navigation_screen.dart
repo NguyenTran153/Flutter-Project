c007737aba31245ed910e7c8683b4051f5a9ccb9
NguyenH8 <nguyentran.07052002ak@gmail.com>
2023-10-22 16:32:03 +0700

    Fix homepage

---
+++ lib/screens/View/navigation_screen.dart
@@ -0,0 +1,89 @@
+import "package:flutter/material.dart";
+import "package:flutter_project/screens/History/history_screen.dart";
+import "package:flutter_project/screens/Homepage/homepage.dart";
+
+class NavigationScreen extends StatefulWidget {
+  const NavigationScreen({super.key});
+
+  @override
+  State<NavigationScreen> createState() => _NavigationScreenState();
+}
+
+class _NavigationScreenState extends State<NavigationScreen> {
+  List<Widget> pages = [
+    const HomepageScreen(),
+    const HomepageScreen(),
+    const HistoryScreen(),
+    const HomepageScreen(),
+    const HomepageScreen(),
+  ];
+  List<String> pagesTitles = [
+    'Home',
+    'Tutors',
+    'Schedule',
+    'Courses',
+    'Settings'
+  ];
+  int _chosenPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(
+          pagesTitles[_chosenPageIndex],
+          style: Theme.of(context).textTheme.displayMedium,
+        ),
+        actions: _chosenPageIndex == 0
+            ? [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      clipBehavior: Clip.hardEdge,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: Image.asset(
+                        "public/images/avatar.png",
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) =>
+                            const Icon(Icons.person_rounded),
+                      ),
+                    ),
+                  ),
+                )
+              ]
+            : [],
+      ),
+      body: pages[_chosenPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedFontSize: 14,
+        unselectedFontSize: 12,
+        type: BottomNavigationBarType.fixed,
+        onTap: (value) {
+          setState(() {
+            _chosenPageIndex = value;
+          });
+        },
+        elevation: 20,
+        currentIndex: _chosenPageIndex,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Tutors'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.schedule_outlined), label: 'Schedule'),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label: 'Courses'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+      ),
+    );
+  }
+}
